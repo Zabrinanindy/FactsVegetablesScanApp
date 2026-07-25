df7683b30a86f18de159f16ea203cbe396e320f0
Nindyyz <zabrina2004anindya@gmail.com>
2026-07-26 01:19:45 +0700

    Add netlify.toml for Netlify build/publish

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j6d314snko"
+    "revision": "0.6cun4d1m3d8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
